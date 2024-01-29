f7c296cb9f354c2c8d6e10a7811b95594e3f01e0
Ruby <kuo970741@gmail.com>
2024-01-29 17:21:12 +0800

    加入專案檔案。

---
+++ Ajax/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using Ajax.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ajax.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+        public ApiController (MyDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities=_context.Addresses.Select(c=>c.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
